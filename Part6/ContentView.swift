79344e7d0a02ea7e0d0fd6af2910ea5c5e047249
moritamu <moris@r.sannet.ne.jp>
2022-05-04 08:05:23 +0900

    2

--- Part6/ContentView.swift
+++ Part6/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Hello, world!")
+                    .padding()
+                NavigationLink(destination: SecondView().navigationTitle("画面２")) {
+                    Text("SecondViewへ")
+                    
+                }
+            }
+            .navigationTitle("画面１")
+//            Navigatonの中にモディファイアはつける
+        }
     }
 }
 
