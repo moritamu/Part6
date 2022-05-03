5543497563e6f6d480cb6ec0adb378dd0c6afafd
moritamu <moris@r.sannet.ne.jp>
2022-05-04 07:48:48 +0900

    1

---
+++ Part6/ScondView.swift
@@ -0,0 +1,23 @@
+//
+//  ScondView.swift
+//  Part6
+//
+//  Created by 森田晋 on 2022/05/04.
+//
+
+import SwiftUI
+
+struct ScondView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow
+            Text("Second View")
+        }
+    }
+}
+
+struct ScondView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScondView()
+    }
+}
