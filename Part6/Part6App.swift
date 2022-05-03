486da6bd44d6de139a22a5dafe4c70ed9729a04e
moritamu <moris@r.sannet.ne.jp>
2022-05-04 07:42:14 +0900

    Initial Commit

---
+++ Part6/Part6App.swift
@@ -0,0 +1,17 @@
+//
+//  Part6App.swift
+//  Part6
+//
+//  Created by 森田晋 on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct Part6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
